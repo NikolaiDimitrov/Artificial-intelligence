ee8d1302eb4f0476db39e84ef4267fd2db8098d4
Stamenov96 <stefan.stamenov1996@gmail.com>
2020-01-19 01:19:46 +0200

    [Genetic] restructure the genetic algoritm to use Town classes, because we need x and y coordinates not only numbers

--- hw3.cpp
+++ hw3.cpp
@@ -4,13 +4,42 @@
 
 #define TOURNAMENT_NUMBER 5
 #define POPULATION_SIZE 100
+#define UNINITIALIZED_TOWN_NAME -7
+#define NUMBER_OF_ITERATIONS 50000
+#define MUTATION_ON_ITERATIION 10
 using namespace std;
+
+class Town{
+	public:
+	int x;
+	int y;
+	int name;
+
+	Town()
+	{
+		this->x = -1;
+		this->y = -1;
+		this->name = -1;
+	}
+
+	Town(int x, int y, int name)
+	{
+		this->x = x;
+		this->y = y;
+		this->name = name;
+	}
+	friend bool operator== (const Town &t1, const Town &t2);
+};
+
+bool operator== (const Town &t1, const Town &t2) {
+	return t1.name == t2.name || (t1.x == t2.x && t1.y == t2.y);
+};	
 
 class Individual
 {
 	public:
 	int fitness;
-	int* arr;
+	Town* arr;
 	static int size;
 	
 	Individual()
@@ -24,19 +53,19 @@ class Individual
 		delete [] arr;
 	}
 
-	Individual(int* arr,int* target)
+	Individual(Town* arr)
 	{
 		fitness = 0;
-		this->arr = new int[size];
+		this->arr = new Town[size];
 		for(int i=0;i<size;i++)
 		{
 			this->arr[i] = arr[i];
 		}
-		calculateFitness(target); 
+		calculateFitness(); 
 	}
 	Individual(const Individual &ind)
 	{
-		this->arr = new int[size];
+		this->arr = new Town[size];
 		for(int i=0;i<size;i++)
 		{
 			this->arr[i] = ind.arr[i];
@@ -45,7 +74,7 @@ class Individual
 	}
 	Individual& operator=(const Individual& ind)
 	{
-		this->arr = new int[size];
+		this->arr = new Town[size];
 		for(int i=0;i<size;i++)
 		{
 			this->arr[i] = ind.arr[i];
@@ -53,23 +82,54 @@ class Individual
 		this->fitness = ind.fitness;
 		return *this;
 	}
-	void generateRandomValues(int* target)
+	void generateRandomValues()
 	{
 		this->fitness = 0;
-		this->arr = new int[size];
-		getRandomArr(this->arr);
-		calculateFitness(target); 
+		this->arr = new Town[size];
+		getRandomArrOfCities();
+		calculateFitness(); 
 	}
-	void calculateFitness(int* target)
+	void calculateFitness()
 	{
-		for(int i = 0;i < size;i++)
+		for (int i = 0; i < size-1; ++i)
 		{
-			if(arr[i] == target[i])
-			{
-				fitness++;
-			}
+			fitness += (abs(arr[i].x - arr[i+1].x) + abs(arr[i].y - arr[i+1].y)); 
 		}
 	}
+
+	void getRandomArrOfCities()
+	{
+		bool sameTown = false;
+		int randomX;
+		int randomY;
+		int randomName;
+		for (int i = 0; i < Individual::size; ++i)
+		{
+			arr[i].name = UNINITIALIZED_TOWN_NAME;
+			do
+			{
+				sameTown =false;
+				// TO DO random x and Y coordinates not size
+				randomX = rand() % size;
+				randomY = rand() % size;
+				randomName = rand() % size;
+				Town newTown(randomX,randomY, randomName);
+				for (int k = 0; k < i; ++k)
+				{
+					if(arr[k] == newTown)
+					{
+						sameTown = true;
+						break;
+					}
+				}
+				if(!sameTown)
+				{
+					arr[i]=newTown;
+				}
+			}while(arr[i].name == UNINITIALIZED_TOWN_NAME);
+		}
+	}
+
 	static void getRandomArr(int* arr)
 	{
 	bool valueIsTaken = false;
@@ -97,7 +157,7 @@ class Individual
 	}
 	}
  	bool operator > (const Individual& ind) {
-         if(fitness > ind.fitness) {
+         if(fitness < ind.fitness) {
             return true;
          }
          return false;
@@ -105,7 +165,7 @@ class Individual
 	bool operator == (Individual &ind) {
 		for(int i = 0;i< ind.size;i++)
 		{
-			if(this->arr[i] != ind.arr[i])
+			if(!(this->arr[i] == ind.arr[i]))
 				return false;
 		}
 		return true;
@@ -115,7 +175,7 @@ class Individual
 ostream & operator << (ostream &out,  Individual &ind)
 {
 	for(int i=0;i<Individual::size;i++)
-    	out << ind.arr[i] << " ";
+    	out << "(" << ind.arr[i].name << "," << ind.arr[i].x << "," << ind.arr[i].y << ")" << " ";
 	out << "Fitness is " << ind.fitness << endl; 
     return out;
 }
@@ -197,25 +257,25 @@ Individual tournamentSelection(Individual* population)
 
 
 
-bool getGenes(int* firstParrent,int* secondParrent,int* firstParrentGenes,int* secondParrentGenes,int index,bool flag)
+bool getGenes(Town* firstParrent,Town* secondParrent,Town* firstParrentGenes,Town* secondParrentGenes,int index,bool flag)
 {
-	if(firstParrent[index] == -1)
+	if(firstParrent[index].name == -1)
 		return flag;
 
 	firstParrentGenes[index] = firstParrent[index];
-	firstParrent[index] = -1;
+	firstParrent[index].name = -1;
 
 	secondParrentGenes[index] = secondParrent[index];
-	secondParrent[index] = -1;
+	secondParrent[index].name = -1;
 	 	
 	for(int i = 0;i < Individual::size;i++)
 	{
-		if(secondParrentGenes[index] == firstParrent[i])
+		if(secondParrentGenes[index].name == firstParrent[i].name)
 			getGenes(firstParrent,secondParrent,firstParrentGenes,secondParrentGenes,i,flag);
 	}
 	return !flag;
 }
-void cycleCrossover(Individual parent1,Individual parent2,int* child1,int* child2)
+void cycleCrossover(Individual parent1,Individual parent2,Town* child1,Town* child2)
 {
 	bool reverseChildrens = true;
 	for(int i = 0;i < Individual::size;i++)
@@ -226,7 +286,7 @@ void cycleCrossover(Individual parent1,Individual parent2,int* child1,int* child
 			reverseChildrens = getGenes(parent1.arr,parent2.arr,child2,child1,i,reverseChildrens);
  	}
 }
-void onePointCrossover(Individual parent1,Individual parent2,int* child1,int* child2)
+void onePointCrossover(Individual parent1,Individual parent2,Town* child1,Town* child2)
 {
 	int randomNumber = rand() % Individual::size;
 	bool flag = true;
@@ -278,17 +338,17 @@ void mutation(Individual* i)
 			secondGene = rand() % Individual::size;
 
 		}while(randomGene == secondGene);
-		int tmp = i->arr[randomGene];
+		Town tmp = i->arr[randomGene];
 		i->arr[randomGene] = i->arr[secondGene];
 		i->arr[secondGene] = tmp;
 }
 
-void geneticSearch(Individual* population,int size,int* target)
+void geneticSearch(Individual* population,int size)
 {
 	int index = 0;
 	Individual childrens[POPULATION_SIZE+50];
-	int* child1 = new int[size];
-	int* child2 = new int[size];
+	Town* child1 = new Town[size];
+	Town* child2 = new Town[size];
 	for(int i = 0;i < 50;i++)
 	{
 		Individual parent1 = tournamentSelection(population);
@@ -300,8 +360,8 @@ void geneticSearch(Individual* population,int size,int* target)
 		
 		cycleCrossover(parent1,parent2,child1,child2);
 	
-		Individual c1(child1,target);
-		Individual c2(child2,target);
+		Individual c1(child1);
+		Individual c2(child2);
 		
 		
 		
@@ -326,42 +386,31 @@ int main()
 {
 	srand(time(NULL));
 	Individual::size = 20;
-	int target[20];
-	Individual::getRandomArr(target);	
-	cout << "Target: " << endl; 
-	for(int i = 0;i < 20;i++)
-	{
-		cout << target[i] << " ";
-	}
-	cout << endl;
 	Individual population[POPULATION_SIZE];
-	
 	for(int i = 0;i < POPULATION_SIZE;i++)
 	{
-		population[i].generateRandomValues(target);
+		population[i].generateRandomValues();
 	}
+
 	sortPopulation(population);
-	for(int i = 0;i < 30000;i++)
-	{	
-		geneticSearch(population,Individual::size,target);
-	}	
-	for(int i = 0;i < 100;i++)
+	/*for (int i = 0; i < POPULATION_SIZE; ++i)
 	{
 		cout << population[i];
-	}
-	// int arr[10];
-	// int arr2[10];
-	// int asd[10] = {1,2,3,4,5,6,7,8,9,10};
-	// int asd2[10] = {10,9,8,7,6,5,4,3,2,1};
-	// onePointCrossover(asd,asd2,arr,arr2);
-	// for (int i = 0; i < 10; ++i)
-	// {
-	// 	cout << arr[i] << " ";
-	// }
-	// cout << endl;
-	// /*for (int i = 0; i < 10; ++i)
-	// {
-	// 	cout << arr2[i] << " ";
-	// }*/
+	}*/
+	cout << population[0] << endl << endl;
+	for(int i = 0;i < NUMBER_OF_ITERATIONS;i++)
+	{	
+		if(i==10 || i==17859 || i==26578 || i==36554){
+			cout<<population[0];
+		}
+
+		geneticSearch(population,Individual::size);
+		if(i % MUTATION_ON_ITERATIION == 0)
+		{
+			int randomNumber = rand() % POPULATION_SIZE;
+			mutation(&population[randomNumber]);
+		}
+	}	
+		cout << population[0];
 	return 0;
 }	
